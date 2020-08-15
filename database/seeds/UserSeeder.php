796eea10f85534a21ee4592f9123a3129d669dde
Sergiy Pedorenko <pedorenko.mail@gmail.com>
2020-08-15 21:13:21 +0300

    add seed for user move model User.php in directory App\Models

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('User'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'administrator@gmail.com',
+            'password' => Hash::make('Administrator'),
+        ]);
+    }
+}
